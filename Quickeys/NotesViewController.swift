6fb5affed3317415280327c3be96f8c3bcef08de
Alex Rosenfeld <alexrosenfeld10@gmail.com>
2016-12-11 18:30:45 -0500

    Added Wolfram search, modified search to be more modular

--- Quickeys/NotesViewController.swift
+++ Quickeys/NotesViewController.swift
@@ -13,6 +13,11 @@ import Cocoa
 class NotesViewController: NSViewController {
     // Lets and vars
     
+    let GOOGLE_URL = "https://www.google.com/search?q="
+    let GOOGLE_NAME = "Google"
+    let WOLFRAM_URL = "https://www.wolframalpha.com/input/?i="
+    let WOLFRAM_NAME = "Wolfram"
+    
     // Outlets
     @IBOutlet var inputText: NSTextView!
     
@@ -29,6 +34,37 @@ class NotesViewController: NSViewController {
         return txt.addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed)!
     }
     
+    func searchTextOnWebsite(website: String) {
+        // Set our destination url
+        var BASE_URL = ""
+        switch website {
+        case GOOGLE_NAME:
+            BASE_URL = GOOGLE_URL
+            break
+        case WOLFRAM_NAME:
+            BASE_URL = WOLFRAM_URL
+            break
+        default:
+            NSLog("Unknown website string" + website)
+            return
+        }
+        
+        let allText = inputText.attributedString().string
+        var url_text = ""
+        
+        if let selectedText = inputText.attributedSubstring(forProposedRange: inputText.selectedRange(), actualRange: nil)?.string {
+            url_text = urlEscapeText(txt: selectedText)
+        } else {
+            url_text = urlEscapeText(txt: allText)
+        }
+        
+        if let url = URL(string: BASE_URL + url_text), NSWorkspace.shared().open(url) {
+            NSLog("browser opened successfully with google")
+        } else {
+            NSLog("browser failed to open")
+            
+        }
+    }
 }
 
 // Actions extension
@@ -40,20 +76,12 @@ extension NotesViewController {
         NSApplication.shared().terminate(sender)
     }
     
-    @IBAction func searchWithGoogle(_ sender: NSButton) {
-        let allText = inputText.attributedString().string
-        var url_text = ""
-        if let selectedText = inputText.attributedSubstring(forProposedRange: inputText.selectedRange(), actualRange: nil)?.string {
-            url_text = urlEscapeText(txt: selectedText)
-        } else {
-            url_text = urlEscapeText(txt: allText)
-        }
-        
-        if let url = URL(string: "https://www.google.com/search?q=" + url_text), NSWorkspace.shared().open(url) {
-            NSLog("browser opened successfully with google")
-        } else {
-            NSLog("browser failed to open")
-            
-        }
+    @IBAction func searchWithGoogleButtonClick(_ sender: NSButton) {
+        searchTextOnWebsite(website: GOOGLE_NAME)
     }
+    
+    @IBAction func searchWithWolframButtonClick(_ sender: NSButton) {
+        searchTextOnWebsite(website: WOLFRAM_NAME)
+    }
+    
 }
