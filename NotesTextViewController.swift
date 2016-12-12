54fbb75c6ecba9e0ff16f74684ac579b6873ba51
Alex Rosenfeld <alexrosenfeld10@gmail.com>
2016-12-12 15:02:29 -0500

    Escape key functionality

---
+++ NotesTextViewController.swift
@@ -0,0 +1,23 @@
+//
+//  NotesTextViewController.swift
+//  Quickeys
+//
+//  Created by Alex Rosenfeld on 12/12/16.
+//  Copyright © 2016 Alex Rosenfeld. All rights reserved.
+//
+
+import Cocoa
+
+class NotesTextViewController: NSTextView {
+    
+    let appDelegate = NSApplication.shared().delegate as! AppDelegate
+    
+    override open func keyDown(with event: NSEvent) {
+        if (event.keyCode == 53){
+            appDelegate.togglePopover(sender: nil)
+        } else {
+            super.keyDown(with: event)
+        }
+        
+    }
+}
